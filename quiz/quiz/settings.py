7ea1653628900a8aa9404a09a2f14cddcf3b81cd
ii-san <mhdihsan24@gmail.com>
2022-11-22 17:36:45 +0700

    create login and logut forms

--- quiz/quiz/settings.py
+++ quiz/quiz/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'quizes',
     'questions',
     'results',
+
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -129,7 +132,12 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = 'index'
--- quiz/quiz/urls.py
+++ quiz/quiz/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('quizes.urls'))
+    path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='auth/logout.html'), name='logout'),
+    path('', include('quizes.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
