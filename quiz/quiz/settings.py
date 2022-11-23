ffa7b8d0bfe5a5bc7a8ca6f8c9910b9c8b497932
ii-san <mhdihsan24@gmail.com>
2022-11-23 18:48:43 +0700

    update commit

--- quiz/quiz/settings.py
+++ quiz/quiz/settings.py
@@ -140,4 +140,6 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'index'
+
--- quiz/quizes/views.py
+++ quiz/quizes/views.py
@@ -1,86 +1,99 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import JsonResponse
 from questions.models import Question, Answer
 from results.models import Result
 from .models import Quiz
 
 
-class QuizesView(ListView):
+class QuizesView(LoginRequiredMixin, ListView):
     model = Quiz
     template_name = 'quizList.html'
     context_object_name = 'quiz'
 
 
 def quizView(request, pk):
-    quiz = Quiz.objects.get(pk=pk)
+    user = request.user
+    if user.is_authenticated:
+        quiz = Quiz.objects.get(pk=pk)
 
-    context = {
-        'obj': quiz
-    }
-    return render(request, 'quiz.html', context)
+        context = {
+            'obj': quiz
+        }
+        return render(request, 'quiz.html', context)
+    else:
+        return redirect('login')
 
 
 def quizDataView(request, pk):
-    quiz = Quiz.objects.get(pk=pk)
-    questions = []
+    user = request.user
+    if user.is_authenticated:
+        quiz = Quiz.objects.get(pk=pk)
+        questions = []
 
-    for q in quiz.get_questions():
-        answers = []
-        for a in q.get_answers():
-            answers.append(a.text)
-        questions.append({str(q): answers})
-    return JsonResponse({
-        'data': questions,
-        'time': quiz.time,
-    })
+        for q in quiz.get_questions():
+            answers = []
+            for a in q.get_answers():
+                answers.append(a.text)
+            questions.append({str(q): answers})
+        return JsonResponse({
+            'data': questions,
+            'time': quiz.time,
+        })
+    else:
+        return redirect('login')
 
 
 def quizSaveView(request, pk):
-    # print(request.POST)
-    if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
-        questions = []
-        data = request.POST
-        data_ = dict(data.lists())
+   user = request.user
+   if user.is_authenticated:
+       # print(request.POST)
+       if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
+           questions = []
+           data = request.POST
+           data_ = dict(data.lists())
 
-        data_.pop('csrfmiddlewaretoken')
+           data_.pop('csrfmiddlewaretoken')
 
-        for k in data_.keys():
-            print('key: ', k)
-            question = Question.objects.get(text=k)
-            questions.append(question)
-        print(questions)
+           for k in data_.keys():
+               print('key: ', k)
+               question = Question.objects.get(text=k)
+               questions.append(question)
+           print(questions)
 
-        user = request.user
-        quiz = Quiz.objects.get(pk=pk)
+           user = request.user
+           quiz = Quiz.objects.get(pk=pk)
 
-        score = 0
-        multiplier = 100 / quiz.numbers_of_questions
-        results = []
-        correct_answer = None
+           score = 0
+           multiplier = 100 / quiz.numbers_of_questions
+           results = []
+           correct_answer = None
 
-        for q in questions:
-            a_selected = request.POST.get(q.text)
+           for q in questions:
+               a_selected = request.POST.get(q.text)
 
-            if a_selected != "":
-                question_answers = Answer.objects.filter(question=q)
-                for a in question_answers:
-                    if a_selected == a.text:
-                        if a.correct:
-                            score += 1
-                            correct_answer = a.text
-                    else:
-                        if a.correct:
-                            correct_answer = a.text
+               if a_selected != "":
+                   question_answers = Answer.objects.filter(question=q)
+                   for a in question_answers:
+                       if a_selected == a.text:
+                           if a.correct:
+                               score += 1
+                               correct_answer = a.text
+                       else:
+                           if a.correct:
+                               correct_answer = a.text
 
-                results.append({str(q): {'correct_answer': correct_answer, 'answered': a_selected}})
-            else:
-                results.append({str(q): 'not answered'})
+                   results.append({str(q): {'correct_answer': correct_answer, 'answered': a_selected}})
+               else:
+                   results.append({str(q): 'not answered'})
 
-        score_ = score * multiplier
-        Result.objects.create(quiz=quiz, user=user, score=score_)
+           score_ = score * multiplier
+           Result.objects.create(quiz=quiz, user=user, score=score_)
 
-        if score_ >= quiz.required_score_to_pass:
-            return JsonResponse({'passed': True, 'score': score_, 'results': results})
-        else:
-            return JsonResponse({'passed': False, 'score': score_, 'results': results})
+           if score_ >= quiz.required_score_to_pass:
+               return JsonResponse({'passed': True, 'score': score_, 'results': results})
+           else:
+               return JsonResponse({'passed': False, 'score': score_, 'results': results})
+   else:
+       return redirect('login')
